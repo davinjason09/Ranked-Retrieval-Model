3a040d3ef84cba2683cfeb28d4243bd383ac63eb
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-09-23 23:33:07 +0700

    feat(Main): added logging

--- query-likelihood/src/Main.cpp
+++ query-likelihood/src/Main.cpp
@@ -1,13 +1,43 @@
 #include "InvertedIndex.h"
+#include <atomic>
 #include <iostream>
+#include <thread>
+
+std::atomic<bool> indexReady(false);
+double indexTime;
+double queryTime;
+
+void buildIndex(InvertedIndex *index) {
+  auto start = std::chrono::high_resolution_clock::now();
+  index->createIndex();
+  auto end = std::chrono::high_resolution_clock::now();
+
+  indexTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                  .count();
+  indexReady = true;
+}
 
 int main(int argc, char *argv[]) {
-  InvertedIndex indexNew("../../data/News.csv");
-  indexNew.createIndex();
+  InvertedIndex index("../../data/News.csv");
+
+  std::thread indexThread(buildIndex, &index);
 
   std::string query;
   std::cout << "Enter your query: ";
   std::getline(std::cin, query);
 
-  indexNew.executeQuery(query);
+  if (!indexReady)
+    std::cout << "Index is not ready yet. Please wait.\n";
+
+  indexThread.join();
+
+  auto start = std::chrono::high_resolution_clock::now();
+  index.executeQuery(query);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  queryTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                  .count();
+
+  std::cout << "\nIndex created in " << indexTime << " ms\n";
+  std::cout << "Query executed in " << queryTime << " ms\n";
 }
